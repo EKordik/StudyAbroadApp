84a28d32380eb477f0052d674d60bc3e5104d6ea
Emma Kordik <emma@gbsvoice.com>
2016-02-23 10:21:02 -0600

    Calendar Functionality Added

--- public_html/js/calendar.js
+++ public_html/js/calendar.js
@@ -1,20 +1,48 @@
 (function($, window, document){
 $(function(){
-    var oneDay = 24*60*60*1000;
     var today = new Date();
-    var startDate = new Date("2016-02-18");
-    
-    var day = Math.round(Math.abs((startDate.getTime() - today.getTime())/(oneDay)));
-    
+
+    var day = dateDiff(today);
     console.log(day);
     
-    $('#day' + day).show();
+    showDate(day);
+    
+    function showDate(day){
+        var numDays = 12;
+        if(day < 0){
+            $('#packingList').show();
+        }else if(day > numDays){
+            $('#thankMsg').show();
+        }else{
+            $('#day' + day).show();
+        }
+    }
+    
     
     $('.showCalendar').click(function(){
-        $('#day' + day).hide();
-        $('#calendar').show();
+        $('.day').hide();
+        $('.calendar').show();
     });
     
+
+  $('input').change(function(){
+      var datePicked = $('input').val();
+        console.log(datePicked);
+        day = dateDiff(new Date(datePicked));
+        console.log(day);
+        showDate(day);
+        $('.calendar').hide();
+  });
+
+    
+    
+    //Calculates the difference of two days to tell us which day we are on.
+    function dateDiff(date){
+        var oneDay = 24*60*60*1000*-1;
+        var startDate = new Date("2016-02-18");
+        var day =  Math.round((startDate.getTime() - date.getTime())/(oneDay));
+        return day;
+    }
 });
 }(window.jQuery, window, document));
 
